d5b427690da38d7660a830d119d49b932affcf97
RafaeloDuarte <rafaeloduarte@gmail.com>
2020-02-28 18:37:32 -0300

    setup of css materialize library

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <div class="container">
+      <div class="row">
+        <a class="btn-floating btn-large pulse"><i class="material-icons">cloud</i></a>
+      </div>
     </div>
   );
 }
